910d5b317b8750d9370f8fe2d174f11bc8aa5293
Macklin Zhong <mackboi@macklins-macbook-pro.local>
2019-01-12 01:33:28 -0800

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Macklin Zhong on 1/12/19.
+//  Copyright © 2019 Macklin Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
